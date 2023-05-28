0610f48a57ca3747fa79985c2059c4baf844f500
Abiodun-Onajobi Treasure <ifethayo2020@gmail.com>
2023-05-28 14:56:30 +0100

    Part 1

--- main.py
+++ main.py
@@ -1,36 +1,28 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-starting_position = [(0,0), (-20, 0), (-40, 0)]
+snake = Snake()
+food = Food()
 
-for position in starting_position:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.goto(position)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-# segment_1 = Turtle("square")
-# segment_1.color("white")
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-# segment_2 = Turtle("square")
-# segment_2.color("white")
-# segment_2.goto(-20, 0)
-
-# segment_2 = Turtle("square")
-# segment_2.color("white")
-# segment_2.goto(-40, 0)
-
-
-
-
-
-
-
-
-
+    snake.move()
 
 
 screen.exitonclick()
